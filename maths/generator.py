0298fde990a848dae6e944f6938961877e90fcd0
TheCazzXz <65710413+TheCrazzXz@users.noreply.github.com>
2023-05-22 15:29:01 -0400

    Delete maths directory

--- maths/generator.py
+++
@@ -1,9 +0,0 @@
-for x in range(4):
-	for y in range(4):
-		for z in range(4):
-			print("m"+str(4 * x + z)+"p(matA) * m"+str(4 * z + y)+"p(matB)", end="")
-			if z < 3:
-				print(" + ", end="")
-		print(",\n")
-
-	print("\n")
